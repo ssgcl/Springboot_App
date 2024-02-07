f84ef1dfd903e78aa25d7a703dd37e27e9a32664
Codedestructor56 <richardfeynman438@gmail.com>
2024-02-07 16:34:41 +0500

    Readability Improvements

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,10 +2,10 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//Initializing springboot app
 @SpringBootApplication
 public class DemoApplication {
-
+	//standard boilerplate
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
--- src/main/java/com/example/demo/HelloWorld.java
+++ src/main/java/com/example/demo/HelloWorld.java
@@ -4,10 +4,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+//Initializing springboot app
 @SpringBootApplication
+//Initializing RESTful services
 @RestController
 public class HelloWorld {
-
+    //Initializing endpoint	
     @GetMapping("/hello")
     public String sayHello() {
         return "Hello, World!";
